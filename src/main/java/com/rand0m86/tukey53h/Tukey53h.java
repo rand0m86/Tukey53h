4d9bed2a8ca32f5da9082809dc5ac853663969e1
rand0m <vetal.alien@gmail.com>
2014-06-08 21:24:54 +0300

    Misprint fixed

--- src/main/java/com/rand0m86/tukey53h/Tukey53h.java
+++ src/main/java/com/rand0m86/tukey53h/Tukey53h.java
@@ -12,7 +12,7 @@ import com.rand0m86.tukey53h.wrapper.NumberWrapper;
 /**
  * Implementation of algorithm named Tukey53h. It's used for filtration of data but it causes some data loss.
  * This data loss is 8 elements long. First 4 elements are lost in the first iteration over data, next 2
- * elements are lost in the second iteration and 2 elements are lost while using Hamming filter.
+ * elements are lost in the second iteration and 2 elements are lost while using Hanning filter.
  *   
  * @author rand0m86
  *
